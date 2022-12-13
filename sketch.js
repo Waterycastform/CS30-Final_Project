019ede424306e5c508dc34bc1f3a24dc787d2a18
Waterycastform <114016086+Waterycastform@users.noreply.github.com>
2022-12-13 12:17:28 -0600

    trail players

--- sketch.js
+++ sketch.js
@@ -14,13 +14,22 @@
 //   background(220);
 // }
 
-let dummy;
+let player1, player2, ground;
 
 function setup() {
-  new Canvas(windowWidth, windowHeight);
+  new Canvas(600, 500);
+  world.gravity.y = 9.8;
+  allSprites.bounciness = 0;
 
-  dummy = new Sprite();
-  dummy.diameter = 50;
+  player1 = new Sprite(150, 150, 50, "dynamic");
+  player2 = new Sprite(250, 250, 50, "dynamic");
+
+  ground = new Sprite();
+  ground.x = width/2;
+  ground.y = height*0.9 + ground.h;
+  ground.w = width;
+  ground.h = height*0.1;
+  ground.collider = "static";
 }
 
 function draw() {
@@ -29,20 +38,29 @@ function draw() {
 }
 
 function spriteMove() {
-  if (kb.pressing('left')) {
-    dummy.vel.x = -3;
+  if (kb.pressing("a")) {
+    player1.vel.x = -3;
   }
-  else if (kb.pressing('right')) {
-    dummy.vel.x = 3;
+  else if (kb.pressing("d")) {
+    player1.vel.x = 3;
   }
-  else if (kb.pressing('up')) {
-    dummy.vel.y = -3;
-  }
-  else if (kb.pressing('down')) {
-    dummy.vel.y = 3;
+  else if (kb.presses("w")) {
+    player1.vel.y = -3;
   }
   else {
-    dummy.vel.x = 0;
-    dummy.vel.y = 0;
+    player1.vel.x = 0;
+  }
+
+  if (kb.pressing("g")) {
+    player2.vel.x = -3;
+  }
+  else if (kb.pressing("j")) {
+    player2.vel.x = 3;
+  }
+  else if (kb.presses("y")) {
+    player2.vel.y = -3;
+  }
+  else {
+    player2.vel.x = 0;
   }
 } 
