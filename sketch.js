d6244bd202dd9d9e33e4a9bd2f9b18ad4c7ba023
Waterycastform <114016086+Waterycastform@users.noreply.github.com>
2022-12-16 12:17:15 -0600

    movement update

--- sketch.js
+++ sketch.js
@@ -6,14 +6,6 @@
 // - learning and using p5.play extention
 
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
-
-// function draw() {
-//   background(220);
-// }
-
 let player1, player2, ground, demoBox;
 
 function setup() {
@@ -46,9 +38,13 @@ function demoWorld() {
 function createPlayers() {
   player1 = new Sprite(150, 150, 25, 60,);
   player1.rotationLock = true;
+  player1.friction = 5;
+  
 
   player2 = new Sprite(250, 250, 25, 60,);
   player2.rotationLock = true;
+  player2.friction = 5;
+  player2.mass = 10;
   player2.overlap(player1); 
 }
 
@@ -56,26 +52,21 @@ function playerMove() {
   if (kb.pressing("a")) {
     player1.vel.x = -3;
   }
-  else if (kb.pressing("d")) {
+  if (kb.pressing("d")) {
     player1.vel.x = 3;
   }
-  else if (kb.presses("w")) {
+  if (player1.colliding(ground) && kb.presses("w")) {
     player1.vel.y = -5;
-  }
-  else {
-    player1.vel.x = 0;
   }
 
   if (kb.pressing("g")) {
     player2.vel.x = -3;
   }
-  else if (kb.pressing("j")) {
+  if (kb.pressing("j")) {
     player2.vel.x = 3;
   }
-  else if (kb.presses("y")) {
+  else if (player2.colliding(ground) && kb.presses("y")) {
     player2.vel.y = -5;
   }
-  else {
-    player2.vel.x = 0;
-  }
+  console.log(player1.x);
 } 
