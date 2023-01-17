b2949833f5716994d2662da4ad45ff4ebdc571e9
Waterycastform <114016086+Waterycastform@users.noreply.github.com>
2023-01-17 12:17:06 -0600

    death block works, collider issue

--- sketch.js
+++ sketch.js
@@ -45,10 +45,11 @@ class Button {
 
 function setup() {
   
-  new Canvas(800, 700);
+  new Canvas(700, 700);
   world.gravity.y = 9.8;
   allSprites.bounciness = 0;
-  createCanvas(800, 700);
+  allSprites.collider = "static";
+  createCanvas(700, 700);
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
   strokeWeight(4);
@@ -78,6 +79,7 @@ function draw() {
   else if (state === "game") {
     
     demoWorld();
+    update();
   }
   
   lastState = state;
@@ -86,7 +88,7 @@ function draw() {
 function demoWorld() {
   if (lastState === "menu") {
     createPlayers();
-    newBox();
+    // newBox();
     map1();
     deathBlock();
   }
@@ -134,43 +136,40 @@ function createPlayers() {
   player1 = new Sprite(40, 450, 25, 60);
   player1.rotationLock = true;
   player1.friction = 5;
+  player1.collider = "dynamic";
+  
   
 
-  player2 = new Sprite(250, 250, 25, 60);
-  player2.rotationLock = true;
-  player2.friction = 5;
-  player2.overlap(player1); 
+  // player2 = new Sprite(40, 450, 25, 60);
+  // player2.rotationLock = true;
+  // player2.friction = 5;
+  // player2.collider = "dynamic";
+  // player2.overlap(player1); 
 }
 
-function newBox() {
-  demoBox = new Sprite(300, 300, 40, 40);
-  demoBox.mass = 25;
-}
+// function newBox() {
+//   demoBox = new Sprite(300, 300, 40, 40);
+//   demoBox.mass = 25;
+//   demoBox.collider = "dynamic";
+// }
 
 function map1() {
 
   boundaries();
 
-  floors = new Group();
-  floors.y = height;
-  floors.h = height*0.07;
-  floors.w = 100;
-  floors.collider = "static";
+  ground = new Sprite();
+  ground.x = width/2;
+  ground.y = height;
+  ground.w = width;
+  ground.h = height*0.1;
+  ground.collider = "static";
 
-  for (let i = 0; i < 3; i++) {
-    ground = new floors.Sprite();
-  }
-
-  floors[0].x = width/4;
-  floors[1].x = width*0.5;
-  floors[2].x = width*0.7;
 
   plat1 = new Sprite();
   plat1.x = plat1.w;
   plat1.w = width/5;
   plat1.y = height*0.75;
   plat1.h = height*0.05;
-  plat1.collider = "static";
 
 }
 
@@ -180,21 +179,18 @@ function boundaries() {
   walls.y = height/2;
   walls.w = height*0.07;
   walls.h = height;
-  walls.collider = "static";
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 3; i++) {
     wall = new walls.Sprite();
   }
 
   walls[0].x = 0;
   walls[1].x = width;
 
-  celining = new Sprite();
-  celining.x = width/2;
-  celining.y = 0;
-  celining.w = width;
-  celining.h = height*0.07;
-  celining.collider = "static";
+  walls[2].x = width/2;
+  walls[2].y = 0;
+  walls[2].rotation = 90;
+
 }
 
 function deathBlock() {
@@ -202,10 +198,14 @@ function deathBlock() {
   dead.x = width*0.8;
   dead.y = height*0.8;
   dead.collider = "s";
+  dead.update();
+}
 
-  // if (player1.collides(dead)) {
-    
-  // }
+function update() {
+  if (dead.collides(player1)) {
+    allSprites.remove();
+    state = "rules";
+  }
 }
 
 function playerMove() {
@@ -223,16 +223,16 @@ function playerMove() {
   }
 
 
-  if (kb.pressing("g")) {
-    player2.vel.x = -3;
-  }
-  else if (kb.pressing("j")) {
-    player2.vel.x = 3;
-  } 
-  else if (player2.vel.y !== 0) {
-    player2.vel.x = 0;
-  }
-  if (player2.vel.y === 0 && kb.presses("y")) {
-    player2.vel.y = -5;
-  }
+  // if (kb.pressing("g")) {
+  //   player2.vel.x = -3;
+  // }
+  // else if (kb.pressing("j")) {
+  //   player2.vel.x = 3;
+  // } 
+  // else if (player2.vel.y !== 0) {
+  //   player2.vel.x = 0;
+  // }
+  // if (player2.vel.y === 0 && kb.presses("y")) {
+  //   player2.vel.y = -5;
+  // }
 } 
